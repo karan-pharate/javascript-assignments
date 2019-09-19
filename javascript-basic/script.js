ed9a63a082c6e287059dfbe23fc3ba9108764e5c
karan-pharate <karan.pharate@codeastu.com>
2019-09-19 20:44:52 +0530

    adds variables and operators

--- javascript-basic/script.js
+++ javascript-basic/script.js
@@ -2,7 +2,13 @@ var x,y,z;
 x = 5;
 y = 6;
 z = x + y;
+carName = "Mushtang";
+x += y;
 document.getElementById('demo1').innerHTML =
  "The value of z is " + z + "." ;
  document.getElementById('demo2').innerHTML =
  "hello world";
+ document.getElementById('demo3').innerHTML =
+ carName;
+ document.getElementById('demo4').innerHTML =
+ x;
