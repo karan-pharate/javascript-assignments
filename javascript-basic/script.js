317b7fb7693c26f2c3109293fab80e6c34bd2491
karan-pharate <karan.pharate@codeastu.com>
2019-09-19 20:37:50 +0530

    adds javascript statements

---
+++ javascript-basic/script.js
@@ -0,0 +1,8 @@
+var x,y,z;
+x = 5;
+y = 6;
+z = x + y;
+document.getElementById('demo1').innerHTML =
+ "The value of z is " + z + "." ;
+ document.getElementById('demo2').innerHTML =
+ "hello world";
