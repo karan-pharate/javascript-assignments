5c937f5d29a10ea507cff397e620bb47907640c2
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 13:01:41 +0530

    adds array methods push,pop,shift,unshift

---
+++ arrays/script.js
@@ -0,0 +1,10 @@
+var a = ["A", "B", "C", "D"];
+a.push("E");
+a.pop();
+a.shift();
+a.unshift("A");
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+document.getElementById('demo1').innerHTML = a;
+document.getElementById('demo2').innerHTML = fruits.toString();
+
