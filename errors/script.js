c223811b4318a38445068227b039a0ca7eef2516
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 17:57:47 +0530

    code formatting

--- errors/script.js
+++ errors/script.js
@@ -1,19 +1,17 @@
 function myFunction() {
-var message, x;
-message = document.getElementById('msg');
-message.innerHTML = "";
-x = document.getElementById('demo').value   ;
-try {   
+  var message, x;
+  message = document.getElementById("msg");
+  message.innerHTML = "";
+  x = document.getElementById("demo").value;
+  try {
     if (x == "") throw "Empty";
     if (isNaN(x)) throw "not a number";
     x = Number(x);
-    if(x < 5) throw "too low";
-    if(x > 10) throw "too high";
-}
-catch(err) {
-message.innerHTML = "Input is "+ err;
-}
-finally {
+    if (x < 5) throw "too low";
+    if (x > 10) throw "too high";
+  } catch (err) {
+    message.innerHTML = "Input is " + err;
+  } finally {
     document.getElementById("demo").value = "";
   }
 }
