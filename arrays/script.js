823a5f0fd5f29b7c650adc25fec33c1094c05828
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 14:42:50 +0530

    adds array method to sort numerically

--- arrays/script.js
+++ arrays/script.js
@@ -3,11 +3,16 @@ a.push("E");
 a.pop();
 a.shift();
 a.unshift("A"),
-fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits = ["Banana", "Orange", "Apple", "Mango"],
+points = [40, 100, 1, 5, 25, 10];
+//document.getElementById('demo5').innerHTML = points;
+
 
 document.getElementById('demo1').innerHTML = a;
 document.getElementById('demo2').innerHTML = fruits.toString();
 document.getElementById('demo3').innerHTML = fruits.sort();
 document.getElementById('demo4').innerHTML = fruits.reverse();
-
-
+function myFunction() {
+    points.sort(function(a, b) {return a - b});
+    document.getElementById('demo5').innerHTML = points;        
+}
