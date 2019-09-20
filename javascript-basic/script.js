0b751a9c1cd83777254ba5e2e4edae0d5051f567
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 15:24:33 +0530

    type conversion

--- javascript-basic/script.js
+++ javascript-basic/script.js
@@ -1,19 +1,29 @@
-var x,y,z;
+var x, y, z;
 x = 5;
 y = 6;
 z = x + y;
 carName = "Mushtang";
 x += y;
 
-document.getElementById('demo1').innerHTML =
- "The value of z is " + z + "." ;
- document.getElementById('demo2').innerHTML =
- "hello world";
- document.getElementById('demo3').innerHTML =
- carName;
- document.getElementById('demo4').innerHTML =
- x;
- document.getElementById('demo5').innerHTML =
- x + carName;
- document.getElementById("demo6").innerHTML =
- (x == y) + "<br>" + (x == z);
+document.getElementById("demo1").innerHTML = "The value of z is " + z + ".";
+document.getElementById("demo2").innerHTML = "hello world";
+document.getElementById("demo3").innerHTML = carName;
+document.getElementById("demo4").innerHTML = x;
+document.getElementById("demo5").innerHTML = x + carName;
+document.getElementById("demo6").innerHTML = (x == y) + "<br>" + (x == z);
+
+var e = [];
+document.getElementById("demo7").innerHTML =
+  5 +
+  null +
+  "<br>" +
+  ("5" + null) +
+  "<br>" +
+  ("5" + 2) +
+  "<br>" +
+  ("5" - 2) +
+  "<br>" +
+  "5" * "2" +
+  "<br>" +
+  "5" / "2" +
+  "<br>";
