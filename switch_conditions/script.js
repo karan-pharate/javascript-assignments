d318d7d4649eb0bc7df8602b21405e244af909a5
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 15:10:08 +0530

    code formatting

--- switch_conditions/script.js
+++ switch_conditions/script.js
@@ -1,37 +1,36 @@
 var day;
 
 function myFunction() {
-    var hour = new Date().getHours();
-    var greeting;
-    if (hour < 18) {
-        greeting = "Good Day";
-    }
-    else { 
-        greeting = "Good Evening";  
-    }
-    document.getElementById("demo1").innerHTML = greeting;
+  var hour = new Date().getHours();
+  var greeting;
+  if (hour < 18) {
+    greeting = "Good Day";
+  } else {
+    greeting = "Good Evening";
+  }
+  document.getElementById("demo1").innerHTML = greeting;
 }
-switch(new Date().getDay()) {
-    case 0:
-        day = "Sunday";
-        break;
-    case 1:
-        day = "Monday";
-        break;
-    case 2:
-        day = "Tuesday";
-        break;
-    case 3:
-        day = "Wednesday";
-        break;
-    case 4:
-        day = "Thursday";
-        break;
-    case 5:
-        day = "Friday";
-        break;
-    case 6:
-        day = "Saturday";
-        break;                          
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
 document.getElementById("demo").innerHTML = "Today is " + day;
