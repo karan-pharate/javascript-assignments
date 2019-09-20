76b7ef2d70002f1e65cd7e5ddab2c87be89b4d1f
karan-pharate <karan.pharate@codeastu.com>
2019-09-20 14:08:20 +0530

    adds array methods sort,reverse

--- arrays/script.js
+++ arrays/script.js
@@ -2,9 +2,12 @@ var a = ["A", "B", "C", "D"];
 a.push("E");
 a.pop();
 a.shift();
-a.unshift("A");
+a.unshift("A"),
 fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 document.getElementById('demo1').innerHTML = a;
 document.getElementById('demo2').innerHTML = fruits.toString();
+document.getElementById('demo3').innerHTML = fruits.sort();
+document.getElementById('demo4').innerHTML = fruits.reverse();
+
 
