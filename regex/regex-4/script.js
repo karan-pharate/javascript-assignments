17e35f98d7ff75d715d7118fc0121acde9e44c1d
karan-pharate <karan.pharate@codeastu.com>
2019-10-08 18:32:32 +0530

    added code for escaping special characters

--- regex/regex-4/script.js
+++ regex/regex-4/script.js
@@ -1,4 +1,5 @@
-function myFunction() {
+// \t  \n \r
+const myFunction = () => {
   const str = "Visit \rW3Schools.\tLearn \nJavaScript.",
     patt1 = /\t/,
     patt2 = /\n/,
@@ -8,4 +9,26 @@ function myFunction() {
     result2 = str.search(patt3);
   document.getElementById("demo").innerHTML =
     result + "<br> " + result1 + "<br> " + result2;
+}
+// \.
+const myFunction1 = () => {
+  const str = "come.I will show you how to do it!",
+    patt1 = /\./,
+    result = str.search(patt1);
+  document.getElementById("demo1").innerHTML = result;
+}
+// \*
+const myFunction2 = () => {
+  const str = "why dont we write star as * ?",
+    patt1 = /\*/,
+    result = str.search(patt1);
+  document.getElementById("demo2").innerHTML = result;
+}
+// \\
+const myFunction3 = () => {
+  const str = "Good\\ evening",
+    patt1 = /\\/,
+    result = str.search(patt1);
+  console.log(result);
+  document.getElementById("demo3").innerHTML = result;
 }
